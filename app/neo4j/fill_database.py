0f8281d3f9a0f50bdc0d8bfb2adc59c46529522b
rbenke <robert.benke2@gmail.com>
2022-11-27 15:30:59 +0100

    fill db with artificial data

--- app/neo4j/fill_database.py
+++ app/neo4j/fill_database.py
@@ -1,19 +1,20 @@
 from connector import Neo4jConnection
 from ogb.nodeproppred import NodePropPredDataset
 from tqdm import tqdm
-
-#TODO: Try this, if it wont work (bacause of lack of disc space)  then generate an artificial graph instead.
+import numpy as np
 
 def main():
-    dataset = NodePropPredDataset("ogbn-products", root="/data")[0][0]
+    N_NODES = 1_000_000
+    N_EDGES = 100_000_000
     conn = Neo4jConnection()
-    edges = dataset["edge_index"]
-    num_nodes = dataset["num_nodes"]
+
     def partition(lst, n):
         division = len(lst) / n
         return [lst[round(division * i):round(division * (i + 1))] for i in range(n)]
-    
-    edges = list(zip(edges[0,:].tolist(), edges[1,:].tolist()))
+
+    conn.run_query(f"UNWIND $props as map MATCH CREATE (n) SET n.id=map", {"props": list(range(N_NODES))})
+
+    edges = list(zip(np.randint(0,N_NODES-1).tolist(),np.randint(0,N_NODES-1).tolist())
     for edge_lst in tqdm(partition(edges, 1000)):
         conn.run_query(f"UNWIND $props as map MATCH (n1:Node),(n2:Node) WHERE n1.id=map[0] and n2.id=map[1] CREATE (n1)-[r:REL]->(n2)", {"props": edge_lst})
         
